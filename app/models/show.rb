933db5c4bddc9ad447b3061809b844edaa8dce07
Jeremy Parker <jrobinparker@gmail.com>
2018-07-02 14:54:20 +0000

    automatically backed up by learn

---
+++ app/models/show.rb
@@ -0,0 +1,25 @@
+class Show < ActiveRecord::Base
+  
+  def highest_rating  
+    show = Show.sum(:rating)
+  end 
+
+  def most_popular_show
+  end 
+  
+  def lowest_rating
+  end 
+  
+  def least_popular_show
+  end 
+  
+  def ratings_show
+  end 
+  
+  def popular_show 
+  end 
+  
+  def shows_by_alphabetical_order
+  end
+  
+end 
